b38599131e792bfd233d9abcea8af539d2f1256c
Jibreel Mohamed <98343238+Zofi107@users.noreply.github.com>
2023-01-10 15:41:16 -0600

    transfer from workspace

---
+++ src/graphnode.cpp
@@ -0,0 +1,60 @@
+#include "graphedge.h"
+#include "graphnode.h"
+#include <memory>
+using namespace std;
+
+GraphNode::GraphNode(int id)
+{
+    _id = id;
+}
+
+GraphNode::~GraphNode()
+{
+    //// STUDENT CODE
+    ////
+	//no need to deconstruct
+
+    ////
+    //// EOF STUDENT CODE
+}
+
+void GraphNode::AddToken(std::string token)
+{
+    _answers.push_back(token);
+}
+
+void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
+{
+    _parentEdges.push_back(edge);
+}
+
+void GraphNode::AddEdgeToChildNode(unique_ptr<GraphEdge> edge)
+{
+    _childEdges.push_back(move(edge));
+}
+
+//// STUDENT CODE
+////
+void GraphNode::MoveChatbotHere(ChatBot chatbot)
+{
+    _chatBot = move(chatbot);
+    _chatBot.SetCurrentNode(this);
+}
+
+void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
+{
+    newNode->MoveChatbotHere(move(_chatBot));
+}
+////
+//// EOF STUDENT CODE
+
+GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
+{
+    //// STUDENT CODE
+    ////
+
+    return _childEdges[index].get();
+
+    ////
+    //// EOF STUDENT CODE
+}
